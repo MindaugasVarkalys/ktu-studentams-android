eb665615b6798bc6324bbae9d7fac6c2268f5c44
Eitvidas Puras <PEitvis@gmail.com>
2017-03-03 16:41:16 +0200

    Added error message for invalid username/password. Not final

--- app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
+++ app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
@@ -1,14 +1,43 @@
 package lt.chocolatebar.ktustudentams;
 
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static EditText usernameInput;
+    private static EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usernameInput = (EditText) findViewById(R.id.username);
+        passwordInput = (EditText) findViewById(R.id.password);
+        final Button button = (Button) findViewById(R.id.login);
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonClicked(v);
+                    }
+                }
+        );
+    }
+
+    protected void buttonClicked(View v){
+        if(TextUtils.isEmpty(usernameInput.getText().toString()) ||
+                TextUtils.isEmpty(passwordInput.getText().toString())) {
+            Toast.makeText(this, "Netinkami prisijungimo duomenys", Toast.LENGTH_SHORT).show();
+        }
     }
 }
