1eb63195f7c198568b604fd6a1dc330c3307c013
Eitvidas Puras <PEitvis@gmail.com>
2017-03-06 13:07:15 +0200

    Completed error messages

--- app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
+++ app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
@@ -1,10 +1,7 @@
 package lt.chocolatebar.ktustudentams;
 
-import android.support.v4.text.TextUtilsCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +9,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static EditText usernameInput;
-    private static EditText passwordInput;
+    private EditText usernameInput;
+    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         usernameInput = (EditText) findViewById(R.id.username);
         passwordInput = (EditText) findViewById(R.id.password);
-        final Button button = (Button) findViewById(R.id.login);
+        final Button login = (Button) findViewById(R.id.login);
 
-        button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        buttonClicked(v);
-                    }
-                }
-        );
+        login.setOnClickListener(this::onLoginButtonClick);
     }
 
-    protected void buttonClicked(View v){
-        if(TextUtils.isEmpty(usernameInput.getText().toString()) ||
-                TextUtils.isEmpty(passwordInput.getText().toString())) {
-            Toast.makeText(this, "Netinkami prisijungimo duomenys", Toast.LENGTH_SHORT).show();
+    protected void onLoginButtonClick(View v) {
+        if (usernameInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, R.string.username_error_message, Toast.LENGTH_SHORT).show();
+            usernameInput.requestFocus();
+        } else if (passwordInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, R.string.password_error_message, Toast.LENGTH_SHORT).show();
+            passwordInput.requestFocus();
         }
     }
 }
