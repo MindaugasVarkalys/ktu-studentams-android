f8b546e55ef4058c73fd4a776e933a7a5d1a31f5
Eitvidas Puras <PEitvis@gmail.com>
2017-03-16 20:25:15 +0200

    Added EditorActionListener

--- app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
+++ app/src/main/java/lt/chocolatebar/ktustudentams/MainActivity.java
@@ -3,9 +3,12 @@ package lt.chocolatebar.ktustudentams;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         final Button login = (Button) findViewById(R.id.login);
 
         login.setOnClickListener(this::onLoginButtonClick);
+        passwordInput.setOnEditorActionListener(this::onEditorAction);
+    }
+
+    protected boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if(actionId == EditorInfo.IME_ACTION_GO){
+            onLoginButtonClick(v);
+            return true;
+        }
+        return false;
     }
 
     protected void onLoginButtonClick(View v) {
